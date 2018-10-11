2f8f719187c00256411ccbbc58896c7a45f9f2a3
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2018-10-11 09:09:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,6 +38,8 @@ def over?(board)
 end
 
 def draw?(board)
+  if WIN_COMBINATIONS.any? { |e| (board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]]) && position_taken?(board, win_combination[0]) }
+  
   return full?(board)
 end
 
