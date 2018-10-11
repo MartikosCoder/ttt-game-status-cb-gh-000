7893454bb24f378f0a3c93eef724aac6357b7f19
Martin Beletskyi-Moawad <martbelmoaw@gmail.com>
2018-10-11 09:02:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ def won?(board)
 
   WIN_COMBINATIONS.each do | win_combination |
     if board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]]
-      return true
+      return win_combination
     end
   end
 
